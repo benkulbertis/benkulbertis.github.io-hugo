b54f42e606a8862478b7682384cd261e078ada5a
Ben Kulbertis <ben@kulbertis.org>
2016-09-22 11:59:18 -0600

    Removed fade in on load.

--- themes/benkulbertis/static/js/main.js
+++ themes/benkulbertis/static/js/main.js
@@ -1,33 +1,4 @@
 $(document).ready(function () {
-
-    $.fn.fadeEach = function (delay, callback) {
-        this.each(function () {
-            var $this = $(this);
-            $this.delay(delay * $this.index()).animate({opacity: 1}, 400);
-        }).promise().done(function () {
-            $.isFunction(callback) && callback.call(this);
-        });
-        return this;
-    };
-
-    if (window.innerWidth > 1100) { // AKA not mobile
-        /* Graceful degredation for JS effects */
-        $('.post').css({overflowY: 'hidden'});
-        $('#headers').css({borderBottom: 'none'});
-        $('#headers > *, nav > *, section.post > *').css({opacity: 0});
-
-        /* Yeah, that's right, brush that weak ass fade. - https://i.imgur.com/OTRjYgU.gif */
-        $('#headers > h1, #headers > h2').fadeEach(300, function () {
-            var $post = $('.post');
-            $post.css({overflowY: 'auto'});
-            $post.contents().fadeEach(50, function () {
-                $('#headers').css({borderBottom: '1px dotted #dddddd'});
-                $('nav').contents().fadeEach(50);
-            });
-
-        });
-    }
-
     $(window).resize(function () {
         var localLinks = 'a[href^="/"],a[href^="http://' + (document.location.hostname || document.location.host) + '"]';
         if (window.innerWidth > 1100) { // AKA not mobile
@@ -63,5 +34,4 @@ $(document).ready(function () {
         /* Dynamic Resize */
         $('section').height(window.innerHeight - 40);
     }).resize();
-
 });
